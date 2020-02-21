0c42f3e5c4be5747ae317266e7d3d7d98dfe852d
P-once <60417356+P-once@users.noreply.github.com>
2020-02-21 12:34:15 -0800

    Version 1

---
+++ Pelicula/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Pelicula
+{
+    public class Pelicula
+    {
+        public string Nombre;
+        public string Director;
+        public string Pais;
+        public int Año;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pelicula Peli1 = new Pelicula();
+            Peli1.Nombre = "Jojo Rabbit";
+            Peli1.Año = 2019;
+
+            Pelicula Peli2 = new Pelicula();
+            Peli2.Nombre = "Titanic";
+            Peli2.Año = 1997;
+
+            Console.WriteLine(Peli1.Nombre);
+            Console.WriteLine(Peli1.Año);
+            Console.WriteLine(Peli2.Nombre);
+            Console.WriteLine(Peli2.Año);
+        }
+    }
+}
