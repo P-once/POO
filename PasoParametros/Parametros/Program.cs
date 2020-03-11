90a8ba7cc0d82f4107cde2bf4bacb13deed17938
P-once <60417356+P-once@users.noreply.github.com>
2020-03-10 18:31:55 -0700

    Actividad 6

---
+++ PasoParametros/Parametros/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Parametros
+{
+
+
+        static void CatetoA_ref(ref int A)
+        {
+            A = A*A;
+        }
+
+        static int CatetoB_in(in int B, out Resultado)
+        {
+            Resultado = B*B;
+        }
+
+        static void Raiz(int A, int B, out Resultado)
+        {
+            Resultado = (A + B)^(1/2);
+        }
+    }
+
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 4;
+            int B = 3;
+            int ResB;
+            int C;
+
+           CatetoA_ref(ref A);
+           CatetoB_in(in B, out ResB);
+           Raiz(A, B, out C);
+        }
+    }
+}
